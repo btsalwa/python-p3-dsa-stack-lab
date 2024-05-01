f413471085a0afbabc7de65f05b57deaaaa70c42
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-05-01 15:53:46 +0300

    done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
 class Stack:
+    '''LIFO Stack implementation'''
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=None):
+        '''Initialize Stack with list and limit'''
+        if items is None:
+            self.items = []
+        else:
+            self.items = items
 
-    def isEmpty(self):
-        pass
+        if limit is None:
+            self.limit = float('inf')
+        else:
+            self.limit = limit
 
     def push(self, item):
-        pass
+        '''Push item onto stack, respecting limit'''
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise Exception("Stack is full")
 
     def pop(self):
-        pass
+        '''Pop item off stack, return None if empty'''
+        if len(self.items) > 0:
+            return self.items.pop()
+        else:
+            return None
 
-    def peek(self):
-        pass
-    
     def size(self):
-        pass
+        '''Return number of items in stack'''
+        return len(self.items)
+
+    def isEmpty(self):
+        '''Return True if stack is empty, False otherwise'''
+        return len(self.items) == 0
 
     def full(self):
-        pass
+        '''Return True if stack is full, False otherwise'''
+        return len(self.items) == self.limit
 
-    def search(self, target):
-        pass
+    def search(self, item):
+        if item in self.items:
+            return self.items.index(item)
+        else:
+            return -1
